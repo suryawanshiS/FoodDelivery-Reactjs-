9ce22ea42cfd3aa6205c07a5d16d191d24699d40
suryawanshiS <surajdtis2002@gmail.com>
2024-08-27 10:38:32 +0530

    updated code

--- fooddelivery/frontend/src/App.jsx
+++ fooddelivery/frontend/src/App.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
 import { Route,Routes } from 'react-router-dom'
--- fooddelivery/frontend/src/Pages/Home/Home.jsx
+++ fooddelivery/frontend/src/Pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 
 const Home = () => {
+
+  const [category,setcategory] = useState("ALL");
   return (
     <div>
         <Header />
-        <ExploreMenu />
+        <ExploreMenu category={category} setcategory={setcategory}/>
     </div>
   )
 }
