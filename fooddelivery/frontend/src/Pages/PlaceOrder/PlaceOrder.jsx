eec5a15defa14f630a9d855610ea9bc38f87c44b
suryawanshiS <surajdtis2002@gmail.com>
2024-09-10 00:14:04 +0530

    add some code

--- fooddelivery/frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ fooddelivery/frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,11 @@
 //code 
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../Context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext)
   return (
     <form className='place-order'>
       <div className="place-order-left">
@@ -17,9 +20,35 @@ const PlaceOrder = () => {
           <input type="text" placeholder='city' />
           <input type="text" placeholder='state' />
         </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='phone' />
           
         </div>
         <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+          <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+
+        </div>
 
       </div>
     </form>
