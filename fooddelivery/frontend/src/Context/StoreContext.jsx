064ebf6f50f74cbc9030a03cfd6da09efa106db9
suryawanshiS <surajdtis2002@gmail.com>
2024-10-16 16:49:20 +0530

    add some changes

--- fooddelivery/frontend/src/Context/StoreContext.jsx
+++ fooddelivery/frontend/src/Context/StoreContext.jsx
@@ -1,5 +1,61 @@
-//new code
-import { createContext, useEffect, useState } from "react";
+// //new code
+// import { createContext, useEffect, useState } from "react";
+// import { food_list } from "../assets/assets";
+
+// // Creating and exporting the StoreContext
+// export const StoreContext = createContext(null);
+
+// // Naming the StoreContextProvider function for HMR support
+// const StoreContextProvider = (props) => {
+//   const [cartItems, setCartItems] = useState({});
+
+//   const addToCart = (itemId) => {
+//     if (!cartItems[itemId]) {
+//       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+//     } else {
+//       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+//     }
+//   };
+
+//   const removeFromCart = (itemId) => {
+//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+//   }
+
+//   const getTotalCartAmount = () => {
+//     let totalAmount = 0;
+//     for (const item in cartItems) {
+//       if (cartItems[item] > 0) {
+//         let itemInfo = food_list.find((product) => product._id === item);
+//         totalAmount += itemInfo.price * cartItems[item]
+//       }
+//       return totalAmount;
+
+//     }
+//   }
+
+
+//   const contextValue = {
+//     food_list,
+//     cartItems,
+//     setCartItems,
+//     addToCart,
+//     removeFromCart,
+//     getTotalCartAmount
+//   };
+
+//   return (
+//     <StoreContext.Provider value={contextValue}>
+//       {props.children}
+//     </StoreContext.Provider>
+//   );
+// };
+
+// // Consistent export
+// export default StoreContextProvider;
+
+
+
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 // Creating and exporting the StoreContext
@@ -18,21 +74,29 @@ const StoreContextProvider = (props) => {
   };
 
   const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-  }
+    setCartItems((prev) => {
+      const updatedCart = { ...prev };
+      if (updatedCart[itemId] > 1) {
+        updatedCart[itemId] -= 1;
+      } else {
+        delete updatedCart[itemId]; // Remove item if quantity is 0
+      }
+      return updatedCart;
+    });
+  };
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
       if (cartItems[item] > 0) {
         let itemInfo = food_list.find((product) => product._id === item);
-        totalAmount += itemInfo.price * cartItems[item]
+        if (itemInfo) {
+          totalAmount += itemInfo.price * cartItems[item];
+        }
       }
-      return totalAmount;
-
     }
-  }
-
+    return totalAmount; // Return total amount after loop
+  };
 
   const contextValue = {
     food_list,
@@ -40,7 +104,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
   };
 
   return (
--- fooddelivery/frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ fooddelivery/frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,71 @@
-//new code 
-import React, { useContext } from 'react'
-import './PlaceOrder.css'
-import { StoreContext } from '../../Context/StoreContext'
+// //new code 
+// import React, { useContext } from 'react'
+// import './PlaceOrder.css'
+// import { StoreContext } from '../../Context/StoreContext'
+
+// const PlaceOrder = () => {
+
+//   const {getTotalCartAmount} = useContext(StoreContext)
+//   return (
+//     <form className='place-order'>
+//       <div className="place-order-left">
+//         <p className="title">Delivery Information</p>
+//         <div className="multi-fields">
+//           <input type="text" placeholder='First name' />
+//           <input type="text" placeholder='Last name' />
+//         </div>
+//         <input type="email" placeholder='Email address' />
+//         <input type="text" placeholder='street' />
+//         <div className="multi-fields">
+//           <input type="text" placeholder='city' />
+//           <input type="text" placeholder='state' />
+//         </div>
+//         <div className="multi-fields">
+//           <input type="text" placeholder='Zip code' />
+//           <input type="text" placeholder='Country' />
+//         </div>
+//         <input type="text" placeholder='phone' />
+          
+//         </div>
+//         <div className="place-order-right">
+//         <div className="cart-total">
+//           <h2>Cart Totals</h2>
+//           <div>
+//           <div className="cart-total-details">
+//               <p>Subtotal</p>
+//               <p>${getTotalCartAmount()}</p>
+//             </div>
+//             <hr />
+//             <div className="cart-total-details">
+//               <p>Delivery Fee</p>
+//               <p>${getTotalCartAmount()===0?0:2}</p>
+//             </div>
+//             <hr />
+//             <div className="cart-total-details">
+//               <b>Total</b>
+//               <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+//             </div>
+//           </div>
+//           <button>PROCEED TO PAYMENT</button>
+
+//         </div>
+
+//       </div>
+//     </form>
+//   )
+// }
+
+// export default PlaceOrder
+
+
+
+import React, { useContext } from 'react';
+import './PlaceOrder.css';
+import { StoreContext } from '../../Context/StoreContext';
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
 
-  const {getTotalCartAmount} = useContext(StoreContext)
   return (
     <form className='place-order'>
       <div className="place-order-left">
@@ -15,44 +75,42 @@ const PlaceOrder = () => {
           <input type="text" placeholder='Last name' />
         </div>
         <input type="email" placeholder='Email address' />
-        <input type="text" placeholder='street' />
+        <input type="text" placeholder='Street' />
         <div className="multi-fields">
-          <input type="text" placeholder='city' />
-          <input type="text" placeholder='state' />
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
         </div>
         <div className="multi-fields">
           <input type="text" placeholder='Zip code' />
           <input type="text" placeholder='Country' />
         </div>
-        <input type="text" placeholder='phone' />
-          
-        </div>
-        <div className="place-order-right">
+        <input type="text" placeholder='Phone' />
+      </div>
+
+      <div className="place-order-right">
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
-          <div className="cart-total-details">
+            <div className="cart-total-details">
               <p>Subtotal</p>
               <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button>PROCEED TO PAYMENT</button>
-
+          <button type="button">PROCEED TO PAYMENT</button>
         </div>
-
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
